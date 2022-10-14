abd5281829c1627a20beee239015b3b48ec3b11d
walter <walter.1116@hotmail.com>
2022-10-14 17:01:20 -0300

    first commit

---
+++ src/main/java/br/com/sistemaDeVendas/application/App.java
@@ -0,0 +1,14 @@
+package br.com.sistemaDeVendas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
